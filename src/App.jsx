4bf4c44257f5acacbe8807268a148c47ec430185
Fadia <guelbifedia@gmail.com>
2025-03-05 16:57:47 +0100

    ajout d un nouveau utilisateur

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,16 @@ const App = () => {
   const [loading, setLoading] = useState(true);
 
   //Stocke l'erreur si elle se produit
-  const [error, setError] = useState()
+  const [error, setError] = useState(null)
+  //stocke les informations de formulaires
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [address, setAddress] = useState('');
+  const [hobbies, setHobbies] = useState(['']);
+  const [newHobby, setNewHobby] = useState('');
+ // Message de confirmation après ajout
+  const [confirmationMessage, setConfirmationMessage] = useState('');
 
   // Fonction pour récupérer les utilisateurs depuis l'API
   const fetchUsers = async () => {
@@ -20,17 +29,41 @@ const App = () => {
       setUsers(response.data);
     } catch (err) {
       console.log('Erreur :', err);
+      setError(err.message);
       
     } finally {
       setLoading(false); // On arrête de charger 
     }
   };
 
+  const handleSubmit  = async (e) => {
+    //console.log(e)
+    e.preventDefault()
+    console.log(firstName, lastName,telephone, address,hobbies)
+    try{
+      const addNewUser = await axios.post('http://localhost:8000/api/users',{firstName, lastName,telephone, address,hobbies: hobbies}); 
+      setConfirmationMessage('Utilisateur ajouté avec succès !');
+    }catch (err) {
+      console.log('Erreur :', err);
+      setConfirmationMessage('Une erreur est survenue lors de l\'ajout de l\'utilisateur.');
+    } finally {
+      fetchUsers() // Met à jour la liste des utilisateurs
+    }
+  }
+
+  const handleAddHobby = async(e) => {
+    e.preventDefault()
+    setHobbies ([...hobbies, newHobby]) // Ajoute le nouveau hobby à la liste
+    setNewHobby(['']) // Réinitialise le champ de texte
+  }
+  
+
   useEffect(() => {
     fetchUsers();
   }, []); //signifie qu'on appelle cette fonction une seule fois au chargement du composant
 
   return (
+    <>
     <div className="app-container">
       <h1>Hello users!</h1>
       
@@ -39,10 +72,11 @@ const App = () => {
       {/* Affiche un message d'erreur en cas d'échec */}
       {error && <p>{error}</p>}
       
+      
 
-      {users && 
+      {users && !loading &&
         users.map((user, index) => (
-          <div key={index} className="user-card">
+          <div key={user.id} className="user-card">
             <h3>First Name: {user.firstName}</h3>
             <h4>Last Name: {user.lastName}</h4>
             <h4>Telephone: {user.telephone}</h4>
@@ -58,6 +92,27 @@ const App = () => {
       ) 
     }
     </div>
+    <form onSubmit={handleSubmit}>
+      <input type ="text" placeholder = 'firstName' onChange={(e) => setFirstName (e.target.value) } required/>
+      <input type ="text" placeholder = 'lastName' onChange={(e) => setLastName (e.target.value) } required/>
+      <input type ="text" placeholder = 'telephone' onChange={(e) => setTelephone (e.target.value) } required/>
+      <input type ="text" placeholder = 'address' onChange={(e) =>  handleHobbyChange(e, index) } required/>
+
+      {/* Champs dynamiques pour les hobbies */}
+      <input type ="text" placeholder = 'hobby' onChange={(e) => setNewHobby(e.target.value) } required/>
+      <button type="button" value={newHobby} onClick={handleAddHobby}>Ajouter  hobby</button><br></br>
+       <div>
+          <ul>
+            {hobbies.map((hobby,index) => (
+               <li key={index}>{hobby}</li>
+            )
+          )}
+          </ul>
+
+      </div>
+      <input type="submit" />
+      </form>
+    </>
   );
 };
 
